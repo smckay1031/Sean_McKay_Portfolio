7031e0d4d199e804bbb4b397cf32bc1eef1c9147
smckay1031 <smckay099@gmail.com>
2024-01-18 12:24:11 -0600

    mobile nav toggling

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
+//Toggle icon navbar 
+let navIcon = document.getElementById('nav-button');
+let navBar = document.getElementById('mobile-nav');
+let exitIcon = document.getElementById('exit-nav');
+let navItem = document.getElementById('nav-item')
+
+function openNav() {
+        navBar.style.display = 'flex';
+        navIcon.style.display = 'none';
+        exitIcon.style.display = 'flex';
+}
+
+function exitNav() {
+    navBar.style.display = 'none';
+    navIcon.style.display = 'flex';
+    exitIcon.style.display = 'none';
+}
+
+navIcon.addEventListener('click', openNav);
+exitIcon.addEventListener('click',exitNav);
+navItem.addEventListener('click', exitNav);
+
+
 
 
