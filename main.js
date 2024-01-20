9b798cfa4224c50d9323d54e5878e69395804a5a
smckay1031 <smckay099@gmail.com>
2024-01-20 15:37:53 -0600

    layout changes

--- main.js
+++ main.js
@@ -1,46 +1,36 @@
-//Toggle icon navbar 
-let navIcon = document.getElementById('nav-button');
-let navBar = document.getElementById('mobile-nav');
-let exitIcon = document.getElementById('exit-nav');
-let navItem = document.getElementById('nav-item')
+//Toggle icon navbar
+let navIcon = document.getElementById("nav-button");
+let navBar = document.getElementById("mobile-nav");
+let exitIcon = document.getElementById("exit-nav");
+let navItem = document.getElementById("nav-item");
 
 function openNav() {
-        navBar.style.right = '0px';
-        navIcon.style.display = 'none';
-        exitIcon.style.display = 'flex';
+  navBar.style.right = "0px";
+  navIcon.style.display = "none";
+  exitIcon.style.display = "flex";
 }
 
 function exitNav() {
-    navBar.style.right= '-100%';
-    navIcon.style.display = 'flex';
-    exitIcon.style.display = 'none';
+  navBar.style.right = "-100%";
+  navIcon.style.display = "flex";
+  exitIcon.style.display = "none";
 }
 
-navIcon.addEventListener('click', openNav);
-exitIcon.addEventListener('click',exitNav);
-navItem.addEventListener('click', exitNav);
+navIcon.addEventListener("click", openNav);
+exitIcon.addEventListener("click", exitNav);
+navItem.addEventListener("click", exitNav);
 
-// Change footer Email Icon sub text when touched 
+// Change footer Email Icon sub text when touched
 
-let emailIcon = document.getElementById('email-icon')
-let emailTxt = document.getElementById('Gmail');
-let emailAddress = document.getElementById('gmail-address');
+let emailIcon = document.getElementById("email-icon");
+let emailTxt = document.getElementById("Gmail");
+let emailAddress = document.getElementById("gmail-address");
 
+function toggleEmail() {
+        emailTxt.style.opacity= '0%';
+        document.getElementById('Gmail').innerHTML = "sean.mckay.web.dev@gmail.com";
+        emailTxt.style.opacity= '100%';
 
-function showEmail() {
-    emailTxt.style.display = 'none';
-    emailAddress.style.display = 'block';
     
 }
-
-function hideEmail() {
-    emailTxt.style.display = 'block';
-    emailAddress.style.display = 'none';
-
-}
-
-emailIcon.addEventListener('click', showEmail);
-emailAddress.addEventListener('click', hideEmail);
-
-
-
+emailIcon.addEventListener('click', toggleEmail);
