eb0eb001da063d05d39b9b8c9be72c280d58b3e7
smckay1031 <smckay099@gmail.com>
2024-01-28 11:05:08 -0600

    mobile responisveness made for projects page

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ emailIcon.addEventListener('click', toggleEmail);
 
 const faders = document.querySelectorAll(".fade-in"); 
 const appearOptions = {
-  threshold: 1,
+  threshold: 0,
   rootMargin: '0px 0px -100px 0px'
 };
 
